1ac2c012cf804077ac1c37c237d7da1311a72e6b
Ubuntu <ubuntu@ip-172-31-82-62.ec2.internal>
2018-05-10 14:11:47 +0000

    active

--- bot/urls.py
+++ bot/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
-from django.conf.urls import url,include
+from django.conf.urls import url
 from meal import views
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^keyboard/', views.keyboard )
+    url(r'^keyboard', views.keyboard),
+    url(r'^message', views.message),	
 ]
--- meal/views.py
+++ meal/views.py
@@ -11,19 +11,18 @@ def keyboard(request):
 
 @csrf_exempt    
 def message(request):
-    json_str = ((request.body).decode('urf-8'))#resquest를 한글호환되는 utf8로 변환
-    received_json = json.loads(json_str)#json문자열로 되어있는걸 python 타입으로 변경시킴
-    content_name = received_json['content']#사용자가 보낸 명령어에서 필드명이 content인 항목을 변수저장.
-    requestMode = content_name.encode('utf-8')
-    if requestMode == '선택1' :
-        return JsonResponse(
-            {
-               'message' : {
-                    'text' : "번역할 내용을 다음과 같이 입력해 주세요.(개발중)\n ex)번역 뭐해?, 번역 안녕ㅋㅋ\n 형식을 갖추지 않으면 답변이 나오지 않습니다ㅜㅜ."
-                },
-               'keyboard' : {
-                    'type' : 'buttons', # 텍스트로 입력받기 위하여 키보드 타입을 text로 설정
-		    'buttons': ['school','test']			
-                }
+    json_str = ((request.body).decode('utf-8'))
+    received_json_data = json.loads(json_str)
+    cafeteria_name = received_json_data['content']
+    
+    
+    return JsonResponse({
+            'message': {
+                'text':' 중식 메뉴입니다.'
+            },
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': ['상록원', '그루터기', '아리수', '기숙사식당', '교직원식당']
             }
-        )
+
+        })
