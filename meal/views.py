2e654e5c921646373070868948174e2faade0b08
Ubuntu <ubuntu@ip-172-31-82-62.ec2.internal>
2018-05-10 13:05:45 +0000

    ubun

--- meal/views.py
+++ meal/views.py
@@ -4,30 +4,26 @@ import json
 def keyboard(request):
     return JsonResponse(
         {
-            'type': 'text',
-            'text': '나의 세계에 온걸 환영해!!\n 채팅창에 "소개"라고 적는다면 나에대해서 더 많은걸 알수있어.'
+            'type': 'buttons',
+            'buttons': ['선택1']
         }
     )
 
 @csrf_exempt    
 def message(request):
-    json_str = (request.body).decode('urf-8')#resquest를 한글호환되는 utf8로 변환
+    json_str = ((request.body).decode('urf-8'))#resquest를 한글호환되는 utf8로 변환
     received_json = json.loads(json_str)#json문자열로 되어있는걸 python 타입으로 변경시킴
     content_name = received_json['content']#사용자가 보낸 명령어에서 필드명이 content인 항목을 변수저장.
     requestMode = content_name.encode('utf-8')
-    if requestMode == '소개':
-        return JsonResponse({
-            'message' : {
-                 'photo': {
-                    'url':'http://ishilly.tistory.com/30',
-                    'width': 640,
-                    'height': 480
+    if requestMode == '선택1' :
+        return JsonResponse(
+            {
+               'message' : {
+                    'text' : "번역할 내용을 다음과 같이 입력해 주세요.(개발중)\n ex)번역 뭐해?, 번역 안녕ㅋㅋ\n 형식을 갖추지 않으면 답변이 나오지 않습니다ㅜㅜ."
                 },
-                'text': '안녕 날 소개하지 이름은 황광회\n 취미는 프로그래밍,독서,힙합감상'
-            },
-            'keyboard': {
-                'type' : 'text'
+               'keyboard' : {
+                    'type' : 'buttons', # 텍스트로 입력받기 위하여 키보드 타입을 text로 설정
+		    'buttons': ['school','test']			
+                }
             }
-        })    
-
-# Create your views here.
+        )
