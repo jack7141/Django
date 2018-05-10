a41c0eb3910833bb939c98fbd5b4c2d3d4ed3cb9
Gogh Hwang <ghl92479@gmail.com>
2018-05-10 18:51:41 +0900

    20180510

--- meal/views.py
+++ meal/views.py
@@ -1,12 +1,34 @@
 from django.http import JsonResponse
-
+from django.views.decorators.csrf import csrf_exempt
+import json
 def keyboard(request):
 
     return JsonResponse(
         {
-	'type': 'buttons',
-	'buttons': ['home','school','etc']
+            'type': 'buttons',
+            'buttons': ['이름','나이','키']
         }
     )
+
+@csrf_exempt    
+def message(request):
+    
+    return JsonResponse(
+        {
+            'message': {
+                'text': '나의 세계에 온걸 환영해',
+                'photo': {
+                    'url':'http://ishilly.tistory.com/30',
+                    'width': 640,
+                    'height': 480
+                }
+            },
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': ['이름','나이','키']
+
+            }
+        }
+    )    
 
 # Create your views here.
